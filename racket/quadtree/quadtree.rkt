677ed51d9db52e022118438c3202e47c5a9614bc
Ben Draut <drautb@familysearch.org>
2015-05-05 08:47:38 -0600

    More testing with quadtree.

--- racket/quadtree/quadtree.rkt
+++ racket/quadtree/quadtree.rkt
@@ -2,6 +2,33 @@
 
 ;; This is a study for Greebles in which I implemented a Quadtree
 ;; for fast collision detection.
+;;
+;; A fully-filled Greebles grid contains 23 * 19 = 437 blocks. On
+;; average, I would guess we'll only have about half that: 218.
+;;
+;; With rendering enabled, it can stay up at 60 FPS with up to about
+;; 100 blocks. Any more than that, it starts to deteriorate. When we
+;; get up to about 220 blocks, it hovers at 30 FPS.
+;;
+;; With rendering disabled, we can get up to about 470 blocks before
+;; we start dropping frames. (60 FPS) So now I'm wondering, if I'm
+;; rendering textures, not just lines, is the performance worse? Or
+;; do textures (the same texture) actually perform better?
+;;
+;; It looks like the performance is slightly better when rendering
+;; textures as opposed to just lines. With texture rendering, It
+;; hovers around 38 FPS with 220 blocks, instead of 30 FPS.
+;;
+;; I bumped the block count up to 437 to see how we perfrom at our max.
+;; With texture rendering, we get 18 FPS. With lines, we get 16, so
+;; the difference isn't as great.
+;;
+;; If I disable the collision detection, so we're just doing the
+;; boundary checks and quadtree insertion, we get 23 FPS with
+;; textures, and 21 with lines. Again, fairly close, and just a few
+;; FPS higher than the previous test. So Collision detection isn't
+;; the expensive part. It's the quadtree construction that is the
+;; expensive part.
 
 (require racket/gui
          racket/performance-hint
@@ -9,7 +36,14 @@
          ; profile/render-graphviz)
          profile/render-text)
 
+
+(define NUM-BLOCKS 437)
+(define RENDERING 'lines) ;; Should be 'texture 'lines or 'none
+
 (define BLOCK-SIZE 24)
+
+(define BLOCK-IMG (make-bitmap 1 1))
+(send BLOCK-IMG load-file "block.png")
 
 (define GRID-WIDTH 23)
 (define GRID-HEIGHT 19)
@@ -23,7 +57,6 @@
 (define MAX-OBJECTS 10)
 (define MAX-LEVELS 5)
 
-(define NUM-BLOCKS 250)
 (define MIN-BLOCK-PIXELS-PER-SECOND (* 2 BLOCK-SIZE))
 (define MAX-BLOCK-PIXELS-PER-SECOND (* 10 BLOCK-SIZE))
 
@@ -38,15 +71,18 @@
 (struct block (colliding x-vel y-vel) #:super struct:bbox #:mutable #:transparent)
 
 (define-inline (intersect? bbox-1 bbox-2)
-  (let* ([min-x1 (bbox-x bbox-1)][min-y1 (bbox-y bbox-1)]
-                                 [max-x1 (+ min-x1 (bbox-width bbox-1))][max-y1 (+ min-y1 (bbox-height bbox-1))]
-                                 [min-x2 (bbox-x bbox-2)][min-y2 (bbox-y bbox-2)]
-                                 [max-x2 (+ min-x2 (bbox-width bbox-2))][max-y2 (+ min-y2 (bbox-height bbox-2))])
-    (and (< min-x1 max-x2) (> max-x1 min-x2)
-         (< min-y1 max-y2) (> max-y1 min-y2))))
+               (let* ([min-x1 (bbox-x bbox-1)][min-y1 (bbox-y bbox-1)]
+                      [max-x1 (+ min-x1 (bbox-width bbox-1))][max-y1 (+ min-y1 (bbox-height bbox-1))]
+                      [min-x2 (bbox-x bbox-2)][min-y2 (bbox-y bbox-2)]
+                      [max-x2 (+ min-x2 (bbox-width bbox-2))][max-y2 (+ min-y2 (bbox-height bbox-2))])
+                 (and (< min-x1 max-x2) (> max-x1 min-x2)
+                      (< min-y1 max-y2) (> max-y1 min-y2))))
 
 (define (draw-bbox dc b)
   (send dc draw-rectangle (bbox-x b) (bbox-y b) (bbox-width b) (bbox-height b)))
+
+(define (draw-block dc b)
+  (send dc draw-bitmap BLOCK-IMG (bbox-x b) (bbox-y b)))
 
 ;; Quadtree Node Class
 (define quadtree%
@@ -60,65 +96,65 @@
     (define nodes '())
 
     (define/public (clear)
-      (set! objects '())
-      (map (λ (node)
-             (send node clear))
-           nodes))
+                   (set! objects '())
+                   (map (λ (node)
+                          (send node clear))
+                        nodes))
     (define/public (insert b)
-      (let ([index (get-index b)])
-        (cond [(and (not (empty? nodes)) (not (= -1 index)))
-               (send (list-ref nodes index) insert b)]
-              [else (add-object b)
-                    (when (and (> (length objects) MAX-OBJECTS) (< current-level MAX-LEVELS))
-                      (when (empty? nodes) (split))
-                      (set! objects
-                            (remove* (list null) (map (begin-encourage-inline (λ (n)
-                                                        (let ([idx (get-index n)])
-                                                          (if (= -1 idx)
-                                                              n
-                                                              (begin (send (list-ref nodes idx) insert n) null)))))
-                                                      objects))))])))
+                   (let ([index (get-index b)])
+                     (cond [(and (not (empty? nodes)) (not (= -1 index)))
+                            (send (list-ref nodes index) insert b)]
+                           [else (add-object b)
+                            (when (and (> (length objects) MAX-OBJECTS) (< current-level MAX-LEVELS))
+                              (when (empty? nodes) (split))
+                              (set! objects
+                                    (remove* (list null) (map (begin-encourage-inline (λ (n)
+                                                                                        (let ([idx (get-index n)])
+                                                                                          (if (= -1 idx)
+                                                                                              n
+                                                                                              (begin (send (list-ref nodes idx) insert n) null)))))
+                                                              objects))))])))
     (define/public (get-candidates b)
-      (let ([idx (get-index b)])
-        (if (and (not (= -1 idx)) (not (empty? nodes)))
-            (append objects (send (list-ref nodes idx) get-candidates b))
-            objects)))
+                   (let ([idx (get-index b)])
+                     (if (and (not (= -1 idx)) (not (empty? nodes)))
+                         (append objects (send (list-ref nodes idx) get-candidates b))
+                         objects)))
     (define/public (draw dc)
-      (send dc set-pen "black" 1 'solid)
-      (send dc set-brush "white" 'transparent)
-      (when (and (empty? nodes)
-                 (not (empty? objects)))
-        (draw-bbox dc bounds))
-      (for ([n (in-list nodes)])
-        (send n draw dc)))
+                   (send dc set-pen "black" 1 'solid)
+                   (send dc set-brush "white" 'transparent)
+                   (when (and (empty? nodes)
+                              (not (empty? objects)))
+                     (draw-bbox dc bounds))
+                   (for ([n (in-list nodes)])
+                     (send n draw dc)))
 
     (define/private (split)
-      (let ([new-level (add1 current-level)]
-            [subwidth (/ (bbox-width bounds) 2)]
-            [subheight (/ (bbox-height bounds) 2)]
-            [x (bbox-x bounds)]
-            [y (bbox-y bounds)])
-        (set! nodes
-              (list (new quadtree% [level new-level][bounding-box (bbox x y subwidth subheight)])
-                    (new quadtree% [level new-level][bounding-box (bbox (+ x subwidth) y subwidth subheight)])
-                    (new quadtree% [level new-level][bounding-box (bbox (+ x subwidth) (+ y subheight) subwidth subheight)])
-                    (new quadtree% [level new-level][bounding-box (bbox x (+ y subheight) subwidth subheight)])))))
+                    (let ([new-level (add1 current-level)]
+                          [subwidth (/ (bbox-width bounds) 2)]
+                          [subheight (/ (bbox-height bounds) 2)]
+                          [x (bbox-x bounds)]
+                          [y (bbox-y bounds)])
+                      (set! nodes
+                            (list (new quadtree% [level new-level][bounding-box (bbox x y subwidth subheight)])
+                                  (new quadtree% [level new-level][bounding-box (bbox (+ x subwidth) y subwidth subheight)])
+                                  (new quadtree% [level new-level][bounding-box (bbox (+ x subwidth) (+ y subheight) subwidth subheight)])
+                                  (new quadtree% [level new-level][bounding-box (bbox x (+ y subheight) subwidth subheight)])))))
     (define/private (get-index b)
-      (let* ([mid-x (+ (bbox-x bounds) (/ (bbox-width bounds) 2))]
-             [mid-y (+ (bbox-y bounds) (/ (bbox-height bounds) 2))]
-             [x (bbox-x b)][y (bbox-y b)]
-             [x2 (+ x (bbox-width b))][y2 (+ y (bbox-height b))]
-             [top-half (and (< y mid-y) (< y2 mid-y))]
-             [bottom-half (> y mid-y)]
-             [left-half (and (< x mid-x) (< x2 mid-x))]
-             [right-half (> x mid-x)])
-        (cond [(and top-half left-half) 0]
-              [(and top-half right-half) 1]
-              [(and bottom-half right-half) 2]
-              [(and bottom-half left-half) 3]
-              [else -1])))
+                    (let* ([mid-x (+ (bbox-x bounds) (/ (bbox-width bounds) 2))]
+                           [mid-y (+ (bbox-y bounds) (/ (bbox-height bounds) 2))]
+                           [x (bbox-x b)][y (bbox-y b)]
+                           [x2 (+ x (bbox-width b))][y2 (+ y (bbox-height b))]
+                           [top-half (and (< y mid-y) (< y2 mid-y))]
+                           [bottom-half (> y mid-y)]
+                           [left-half (and (< x mid-x) (< x2 mid-x))]
+                           [right-half (> x mid-x)])
+                      (cond [(and top-half left-half) 0]
+                            [(and top-half right-half) 1]
+                            [(and bottom-half right-half) 2]
+                            [(and bottom-half left-half) 3]
+                            [else -1])))
     (define/private (add-object bbox)
-      (set! objects (cons bbox objects)))))
+                    (set! objects (cons bbox objects)))))
 
 (define my-quadtree (new quadtree%
                          [level 0]
@@ -144,8 +180,8 @@
     (set-bbox-x! b (+ (bbox-x b) (* (block-x-vel b) delta)))
     (set-bbox-y! b (+ (bbox-y b) (* (block-y-vel b) delta)))
     (let* ([x (bbox-x b)][y (bbox-y b)]
-                         [width (bbox-width b)][height (bbox-height b)]
-                         [x2 (+ x width)][y2 (+ y height)])
+           [width (bbox-width b)][height (bbox-height b)]
+           [x2 (+ x width)][y2 (+ y height)])
       (cond [(< x 0)
              (set-bbox-x! b 0)
              (set-block-x-vel! b (* -1 (block-x-vel b)))]
@@ -159,12 +195,13 @@
              (set-bbox-y! b (- WINDOW-HEIGHT height))
              (set-block-y-vel! b (* -1 (block-y-vel b)))]))
     (send my-quadtree insert b))
-  (for ([b (in-list blocks)])
-    (let ([candidates (remove b (send my-quadtree get-candidates b))])
-      (for ([b2 (in-list candidates)])
-        (when (intersect? b b2)
-          (set-block-colliding! b #t)
-          (set-block-colliding! b2 #t))))))
+  ; (for ([b (in-list blocks)])
+  ;   (let ([candidates (remove b (send my-quadtree get-candidates b))])
+  ;     (for ([b2 (in-list candidates)])
+  ;       (when (intersect? b b2)
+  ;         (set-block-colliding! b #t)
+  ;         (set-block-colliding! b2 #t)))))
+  )
 
 (define (draw-framerate dc frames)
   (when (> (length frames) 1)
@@ -178,12 +215,16 @@
 
 ;; Draws the simulation
 (define (draw-world canvas dc)
-  (send my-quadtree draw dc)
-  (send dc set-brush "white" 'transparent)
-  (for ([b (in-list blocks)])
-    (send dc set-pen "blue" 1 'solid)
-    (when (block-colliding b) (send dc set-pen "red" 1 'solid))
-    (draw-bbox dc b))
+  (unless (eq? RENDERING 'none)
+    (when (eq? RENDERING 'lines) (send my-quadtree draw dc))
+    (if (eq? RENDERING 'lines)
+        (begin (send dc set-brush "white" 'transparent)
+               (for ([b (in-list blocks)])
+                 (send dc set-pen "blue" 1 'solid)
+                 (when (block-colliding b) (send dc set-pen "red" 1 'solid))
+                 (draw-bbox dc b)))
+        (begin (for ([b (in-list blocks)])
+                 (draw-block dc b)))))
   (draw-framerate dc frames))
 
 ;; GUI stuff
