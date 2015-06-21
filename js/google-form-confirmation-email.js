7a87ba046588570fe4a6ad786cc33e1fc40fcae5
Ben Draut <drautb@gmail.com>
2015-06-20 22:39:00 -0600

    Add Google apps script to send confirmation email
    This is a JS file that when added to a google form, will send a
    confirmation email to each submitter.

---
+++ js/google-form-confirmation-email.js
@@ -0,0 +1,45 @@
+/*
+  Sends an email confirmation of the home teaching report to the submitter.
+
+  * On the _responses_ spreadsheet, click Tools -> Script Editor.
+  * Copy/paste this code.
+  * Run the "Initialize" function.
+ */
+
+function Initialize() {
+  var triggers = ScriptApp.getProjectTriggers();
+
+  for (var t in triggers) {
+    ScriptApp.deleteTrigger(triggers[t]);
+  }
+
+  ScriptApp.newTrigger("SendConfirmationEmail")
+    .forSpreadsheet(SpreadsheetApp.getActiveSpreadsheet())
+    .onFormSubmit()
+    .create();
+}
+
+
+function SendConfirmationEmail(event) {
+  var pp6Email = Session.getActiveUser().getEmail(),
+      senderName = "West Elder's Quorum - Provo Peak 6th Ward",
+      subject = "Home Teaching Report Received";
+
+  var spreadsheet = SpreadsheetApp.getActiveSheet();
+  var columns = spreadsheet.getRange(1, 1, 1, spreadsheet.getLastColumn()).getValues()[0];
+
+  var sendTo = event.values[7].toString();
+
+  var messageBody = "<html><body><table border=\"1\"><tr><th>Question</th><th>Answer</th></tr>";
+
+  for (var k in columns) {
+    var key = columns[k];
+    if (event.namedValues[key]) {
+      messageBody += "<tr><td width=\"50%\"><strong>" + key + "</strong></td><td width=\"50%\">" + event.namedValues[key] + "</td></tr>";
+    }
+  }
+
+  messageBody += "</table></body></html>";
+
+  MailApp.sendEmail(sendTo, subject, messageBody, {htmlBody: messageBody, cc: pp6Email, name: "West Elder's Quorum - Provo Peak 6th Ward"});
+}
