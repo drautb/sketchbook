bcadb6f2b23f994aa2b6a808a76cae5d6a9d4dd9
Ben Draut <drautb@gmail.com>
2015-06-20 22:39:31 -0600

    Update version fetcher.
    * List artifacts alphabetically.
    * Include common AWS packages.

--- racket/latest-mvn-versions.rkt
+++ racket/latest-mvn-versions.rkt
@@ -7,15 +7,22 @@
          xml/path)
 
 (define HOST "mvn.fsglobal.net")
-(define COLUMN-WIDTH 30)
+(define COLUMN-WIDTH 40)
+
+(define APPROVED-REPO "approved")
+(define PAAS-GROUP "org.familysearch.paas")
+
+(struct artifact (repo group id))
 
 (define ARTIFACTS
-  (list "paas-sps-context"
-        "paas-sps-common"
-        "paas-sps-common-test"
-        "paas-sps-config"
-        "paas-sps-launcher"
-        "paas-sysps-context"))
+  (list (artifact APPROVED-REPO PAAS-GROUP "paas-sps-context")
+        (artifact APPROVED-REPO PAAS-GROUP "paas-sps-common")
+        (artifact APPROVED-REPO PAAS-GROUP "paas-sps-common-test")
+        (artifact APPROVED-REPO PAAS-GROUP "paas-sps-config")
+        (artifact APPROVED-REPO PAAS-GROUP "paas-sps-launcher")
+        (artifact APPROVED-REPO PAAS-GROUP "paas-sysps-context")
+        (artifact "thirdparty" "com.amazonaws" "aws-java-sdk-flow-build-tools")
+        (artifact "central" "com.amazonaws" "aws-java-sdk")))
 
 (define (build-request-path repository group artifact version)
   (string-append "/service/local/artifact/maven?r=" repository "&g=" group "&a=" artifact "&v=" version))
@@ -25,17 +32,18 @@
     (xml->xexpr (document-element (read-xml in-port)))))
 
 (define (fetch-latest-artifact-version artifact)
-  (let* ([path (build-request-path "approved" "org.familysearch.paas" artifact "RELEASE")]
+  (let* ([path (build-request-path (artifact-repo artifact) (artifact-group artifact) (artifact-id artifact) "RELEASE")]
          [response (execute-request path)])
-    (se-path* '(project version) response)))
+    (or (se-path* '(project version) response)
+        (se-path* '(project parent version) response))))
 
 (define (build-format-str a)
-  (string-append "~a" (make-string (- COLUMN-WIDTH (string-length a)) #\space) "~a~n"))
+  (string-append "~a" (make-string (- COLUMN-WIDTH (string-length (artifact-id a))) #\space) "~a~n"))
 
 (define (show-latest-version-for-artifacts artifacts)
-  (for ([a (sort artifacts string<?)])
+  (for ([a (sort artifacts (lambda (a b) (string<? (artifact-group a) (artifact-id b))))])
     (let ([format-str (build-format-str a)])
-      (printf format-str a (fetch-latest-artifact-version a)))))
+      (printf format-str (artifact-id a) (fetch-latest-artifact-version a)))))
 
 (show-latest-version-for-artifacts ARTIFACTS)
 
