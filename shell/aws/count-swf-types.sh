301da8360119334c62ef6e6c2bacde8cf83b08ba
Ben Draut <drautb@gmail.com>
2015-08-19 08:39:31 -0600

    Count SWF types in AWS.

---
+++ shell/aws/count-swf-types.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+DOMAIN="$1"
+
+echo "Counting workflow and activity types in $DOMAIN domain..."
+
+REGISTERED_ACTIVITY_TYPES=$(aws swf list-activity-types --domain $DOMAIN --registration-status REGISTERED | jq '.typeInfos | length')
+DEPRECATED_ACTIVITY_TYPES=$(aws swf list-activity-types --domain $DOMAIN --registration-status DEPRECATED | jq '.typeInfos | length')
+
+echo "\nACTIVITIES:"
+echo "Registered: $REGISTERED_ACTIVITY_TYPES"
+echo "Deprecated: $DEPRECATED_ACTIVITY_TYPES"
+
+REGISTERED_WORKFLOW_TYPES=$(aws swf list-workflow-types --domain $DOMAIN --registration-status REGISTERED | jq '.typeInfos | length')
+DEPRECATED_WORKFLOW_TYPES=$(aws swf list-workflow-types --domain $DOMAIN --registration-status DEPRECATED | jq '.typeInfos | length')
+
+echo "\nWORKFLOWS:"
+echo "Registered: $REGISTERED_WORKFLOW_TYPES"
+echo "Deprecated: $DEPRECATED_WORKFLOW_TYPES"
+
+TOTAL=$((REGISTERED_ACTIVITY_TYPES + DEPRECATED_ACTIVITY_TYPES + REGISTERED_WORKFLOW_TYPES + DEPRECATED_WORKFLOW_TYPES))
+echo "\nTOTAL: $TOTAL"
