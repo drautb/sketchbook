cc8a300502784fe941cfa12a98704310fa2f975a
Ben Draut <drautb@gmail.com>
2016-04-19 15:36:44 -0600

    Update `copy-password.sh` for new portal

--- shell/copy-password.sh
+++ shell/copy-password.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-cat ~/$1-us-east-1-env | grep AWS_PASSWORD | sed -E 's/.*="(.*)"$/\1/g' | pbcopy
+cat ~/$1-env | grep AWS_PASSWORD | sed -E 's/.*="(.*)"$/\1/g' | sed -E 's/\\"/"/g' | pbcopy
