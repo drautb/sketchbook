0b973877b1f646e2eb92b749c9318bb21d525203
Ben Draut <drautb@gmail.com>
2016-11-17 21:20:20 -0700

    Update script

--- python/dnsmasq-hosts-update.py
+++ python/dnsmasq-hosts-update.py
@@ -2,7 +2,7 @@ import requests
 from subprocess import call
 
 ETAG_FILE = '/tmp/dnsmasq-config.etag'
-CONFIG_FILE_URL = 'https://gist.githubusercontent.com/drautb/30827af5945a400cb64811d7f66dc16c/raw'
+HOSTS_FILE_URL = 'https://gist.githubusercontent.com/drautb/30827af5945a400cb64811d7f66dc16c/raw'
 
 call(["touch", ETAG_FILE])
 
@@ -16,19 +16,14 @@ def file_put_contents(filename, contents):
     with open(filename, 'w') as f:
         f.write(contents)
 
-# Begin main logic
-print 'Checking for config updates'
 
 headers = {'If-None-Match': file_get_contents(ETAG_FILE)}
-r = requests.get(CONFIG_FILE_URL, headers=headers)
+r = requests.get(HOSTS_FILE_URL, headers=headers)
 
 if r.status_code == 200:
-    print "Config has changed, replacing current config."
     file_put_contents("./config", r.text)
-    print "Saving ETag for new config."
     file_put_contents(ETAG_FILE, r.headers['ETag'])
-    print "Restarting dnsmasq..."
-elif r.status_code == 304:
-    print "Config hasn't changed."
-else:
+    call(["service", "dnsmasq", "restart"])
+elif r.status_code != 304:
     print "Received unexpected response! " + r
+    exit(1)
