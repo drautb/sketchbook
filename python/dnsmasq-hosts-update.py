ae2e5648f28ee1b0ab024fd49fe5b5d1a2f10b15
Ben Draut <drautb@gmail.com>
2016-11-21 14:39:14 -0700

    Update update script

--- python/dnsmasq-hosts-update.py
+++ python/dnsmasq-hosts-update.py
@@ -2,6 +2,7 @@ import requests
 from subprocess import call
 
 ETAG_FILE = '/tmp/dnsmasq-config.etag'
+LOCAL_HOSTS_FILE = '/etc/hosts'
 HOSTS_FILE_URL = 'https://gist.githubusercontent.com/drautb/30827af5945a400cb64811d7f66dc16c/raw'
 
 call(["touch", ETAG_FILE])
@@ -21,9 +22,9 @@ headers = {'If-None-Match': file_get_contents(ETAG_FILE)}
 r = requests.get(HOSTS_FILE_URL, headers=headers)
 
 if r.status_code == 200:
-    file_put_contents("./config", r.text)
+    file_put_contents(LOCAL_HOSTS_FILE, r.text)
     file_put_contents(ETAG_FILE, r.headers['ETag'])
-    call(["service", "dnsmasq", "restart"])
+    call(["/usr/sbin/service", "dnsmasq", "restart"])
 elif r.status_code != 304:
     print "Received unexpected response! " + r
     exit(1)
