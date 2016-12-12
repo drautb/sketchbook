4b5ff8a6db753b9822c8ecda03b944c4e3d2584b
Ben Draut <drautb@gmail.com>
2016-12-12 15:34:32 -0700

    Update copy-password script

--- shell/copy-password.sh
+++ shell/copy-password.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-cat ~/$1-env | grep AWS_PASSWORD | sed -E 's/.*="(.*)"$/\1/g' | sed -E 's/\\"/"/g' | pbcopy
+< "$HOME/.paas-portal" jq -r .accountCredentials\[\""$1"\"\].password | pbcopy
