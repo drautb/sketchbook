fa1c6cf2fa7f73741dafc58d930b9f2469560012
Ben Draut <drautb@gmail.com>
2017-01-13 12:21:28 -0700

    Update copy-password script again

--- shell/copy-password.sh
+++ shell/copy-password.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-< "$HOME/.paas-portal" jq -r .accountCredentials\[\""$1"\"\].password | pbcopy
+password=$(< "$HOME/.paas-portal" jq -r .accountCredentials\[\""$1"\"\].password)
+system=$(uname -s)
+
+if [ "$system" = "Darwin" ]; then
+  echo "$password" | pbcopy
+elif [ "$system" = "Linux" ]; then
+  copyq add "$password"
+  copyq copy "$password"
+fi
