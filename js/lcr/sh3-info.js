7dfe3e21786ed2cdc28aa05689dedd2de13cf3cb
Ben Draut <drautb@gmail.com>
2017-01-29 20:36:58 -0700

    Update name script

--- js/lcr/sh3-info.js
+++ js/lcr/sh3-info.js
@@ -9,19 +9,39 @@ var fs = require('fs');
 var memberData = JSON.parse(fs.readFileSync('spanish-highlands-3-info.json'));
 var families = memberData['families'];
 
-var names = [];
+var adultNames = [];
+var youthNames = [];
 
 families.forEach(function(f) {
   var headOfHouse = f['headOfHouse'];
   var spouse = f['spouse'];
 
-  names.push(headOfHouse['formattedName']);
+  adultNames.push(headOfHouse['formattedName']);
   if (spouse !== null) {
-    names.push(spouse['formattedName']);
+    adultNames.push(spouse['formattedName']);
   }
+
+  var children = f['children'];
+  children.forEach(function(child) {
+    var birthdate = child['birthdate'];
+
+    var birthday = new Date(birthdate);
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var ageInYears = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    if (ageInYears >= 12) {
+      youthNames.push(child['formattedName']);
+    }
+  });
 });
 
-console.log('ADULT MEMBER LIST: (' + names.length + ')\n');
-names.forEach(function(name) {
+// console.log('ADULT MEMBER LIST: (' + adultNames.length + ')\n');
+// adultNames.forEach(function(name) {
+//   console.log(name);
+// });
+
+console.log('YOUTH MEMBER LIST: (' + youthNames.length + ')\n');
+youthNames.forEach(function(name) {
   console.log(name);
 });
