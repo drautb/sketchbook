f69f9af78e13dd6a885f106ae43736ecfb3233d9
Ben Draut <drautb@gmail.com>
2017-10-02 20:32:34 -0600

    Start sandbox for JCStress

---
+++ java/jcstress-sandbox/src/main/java/io/github/drautb/ConcurrencyTest.java
@@ -0,0 +1,24 @@
+package io.github.drautb;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.IntResult2;
+
+// See jcstress-samples or existing tests for API introduction and testing guidelines
+
+@JCStressTest
+// Outline the outcomes here. The default outcome is provided, you need to remove it:
+@Outcome(id = "0, 0", expect = Expect.ACCEPTABLE, desc = "Default outcome.")
+@State
+public class ConcurrencyTest {
+
+    @Actor
+    public void actor1(IntResult2 r) {
+        // Put the code for first thread here
+    }
+
+    @Actor
+    public void actor2(IntResult2 r) {
+        // Put the code for second thread here
+    }
+
+}
