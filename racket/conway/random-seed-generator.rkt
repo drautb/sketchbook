11a41fe75aedbeb5f6f61ea5627c15de8317d2ff
Ben Draut <drautb@gmail.com>
2017-10-11 13:11:14 -0600

    Finalize random generator

--- racket/conway/random-seed-generator.rkt
+++ racket/conway/random-seed-generator.rkt
@@ -1,20 +1,20 @@
 #lang racket
 
-(define WIDTH 30)
-(define HEIGHT 10)
+(define WIDTH 100)
+(define HEIGHT 75)
+
+(random-seed 0)
 
 (call-with-output-file
-    "random.txt"	 	 	 	 
+    "random.txt"
     (λ (out)
       (define str-contents (make-string (* (add1 WIDTH) HEIGHT) #\.))
       (for ([n (in-range (* (add1 WIDTH) HEIGHT))])
         (if (and (zero? (modulo n (add1 WIDTH)))
                  (> n 0))
             (string-set! str-contents n #\newline)
-            (when (< (random 100) 75)
+            (when (< (random 100) 25)
               (string-set! str-contents n #\X))))
-      (printf "~a~n" str-contents)
       (write-string str-contents out))
   #:mode 'text
   #:exists 'replace)
- 	
