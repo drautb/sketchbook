d1bfebe5b554a3cdd1db3411aeb0e69b8aa43b45
Ben Draut <drautb@gmail.com>
2017-10-11 11:36:04 -0600

    Finished GOL Implementation
    Still want to optimize neighbor counting to see if bitpacking beats
    Jay's time using whole bytes. :)

--- racket/conway/conway.rkt
+++ racket/conway/conway.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
-(require racket/unsafe/ops)
+(require racket/unsafe/ops
+         racket/performance-hint
+         2htdp/image
+         2htdp/universe)
 
 (struct bit-world (rows cols current next) #:mutable)
 
@@ -15,12 +18,12 @@
 
   ;; For each row, iterate over each character. If the character is NOT '.', then flip the bit.
   ;; Need numeric indices for row and colum.
-  (for ([row (in-naturals)]
+  (for ([y (in-naturals)]
         [row-str row-strs])
-    (for ([col (in-naturals)]
+    (for ([x (in-naturals)]
           [c (in-string row-str)])
-      (bit-worldv-set! current cols row col (not (char=? #\. c)))
-      (bit-worldv-set! next cols row col #f)))
+      (bit-worldv-set! current cols x y (not (char=? #\. c)))
+      (bit-worldv-set! next cols x y #f)))
 
   (bit-world rows cols current next))
 
@@ -39,7 +42,7 @@
   (check-eq? (compute-required-bytes 17) 3))
 
 ;; Generate a mask for a particular digit in a byte, with digits counting from left to right. (0 - 7)
-(define (generate-bitmask n)
+(define-inline (generate-bitmask n)
   (define mask 2)
   (cond [(unsafe-fx= n 6) mask]
         [(unsafe-fx< n 6) (unsafe-fxlshift mask (- 6 n))]
@@ -59,9 +62,9 @@
   (make-bytes (compute-required-bytes (* rows cols))))
 
 ;; Set the state for a particular bit.
-(define (bit-worldv-set! bwv cols i j ?)
+(define-inline (bit-worldv-set! bwv cols x y ?)
   ;; Locate the byte, then the bit within the byte, then set it.
-  (define idx (unsafe-fx+ i (unsafe-fx* cols j)))
+  (define idx (unsafe-fx+ x (unsafe-fx* cols y)))
   (define byte-idx (unsafe-fxquotient idx 8))
   (define bit-idx (unsafe-fxmodulo idx 8))
   (define mask (generate-bitmask bit-idx))
@@ -79,44 +82,189 @@
     (check-eq? (unsafe-bytes-ref bwv 3) 0)))
 
 ;; Query the state for particular bit.
-(define (bit-world-ref bwv cols i j)
-  (define idx (unsafe-fx+ i (unsafe-fx* cols j)))
-  (define byte-idx (unsafe-fxquotient idx 8))
-  (define bit-idx (unsafe-fxmodulo idx 8))
-  (not (unsafe-fx= 0 (unsafe-fxand (unsafe-bytes-ref bwv byte-idx) (generate-bitmask bit-idx)))))
+(define-inline (bit-worldv-ref bwv rows cols x y)
+  (cond [(or (unsafe-fx< x 0) (unsafe-fx>= x cols)
+             (unsafe-fx< y 0) (unsafe-fx>= y rows))
+         #f]
+        [else
+         (define idx (unsafe-fx+ x (unsafe-fx* cols y)))
+         (define byte-idx (unsafe-fxquotient idx 8))
+         (define bit-idx (unsafe-fxmodulo idx 8))
+         (not (unsafe-fx= 0 (unsafe-fxand (unsafe-bytes-ref bwv byte-idx) (generate-bitmask bit-idx))))]))
 
 (module+ test
   (let ([bwv (make-bytes 10)])
-    (check-eq? (bit-world-ref bwv 40 12 1) #f))
+    (check-eq? (bit-worldv-ref bwv 2 40 12 1) #f))
   (let ([bwv (make-bytes 10)])
     (unsafe-bytes-set! bwv 7 128)
-    (check-eq? (bit-world-ref bwv 40 16 1) #t)))
+    (check-eq? (bit-worldv-ref bwv 2 40 16 1) #t)))
+
+(define-inline (count-neighbors bwv rows cols x y)
+  (define count 0)
+  (for* ([ny (in-range (sub1 y) (+ 2 y))]
+         [nx (in-range (sub1 x) (+ 2 x))])
+    (unless (and (unsafe-fx= x nx) (unsafe-fx= y ny))
+      (set! count (+ count (if (bit-worldv-ref bwv rows cols nx ny) 1 0)))))
+  count)
+
+(module+ test
+  (let ([bwv (make-bytes 3)]
+        [rows 3]
+        [cols 8])
+    (bit-worldv-set! bwv cols 0 0 #t)
+    (bit-worldv-set! bwv cols 1 0 #t)
+    (bit-worldv-set! bwv cols 2 0 #t)
+    (bit-worldv-set! bwv cols 0 1 #t)
+    (bit-worldv-set! bwv cols 2 1 #t)
+    (bit-worldv-set! bwv cols 0 2 #t)
+    (bit-worldv-set! bwv cols 1 2 #t)
+    (bit-worldv-set! bwv cols 2 2 #t)
+    (check-eq? (count-neighbors bwv rows cols 0 0) 2)
+    (check-eq? (count-neighbors bwv rows cols 1 0) 4)
+    (check-eq? (count-neighbors bwv rows cols 2 0) 2)
+    (check-eq? (count-neighbors bwv rows cols 0 1) 4)
+    (check-eq? (count-neighbors bwv rows cols 1 1) 8)
+    (check-eq? (count-neighbors bwv rows cols 2 1) 4)
+    (check-eq? (count-neighbors bwv rows cols 0 2) 2)
+    (check-eq? (count-neighbors bwv rows cols 1 2) 4)
+    (check-eq? (count-neighbors bwv rows cols 2 2) 2)
+    (check-eq? (count-neighbors bwv rows cols 6 2) 0)))
+
+(define-inline (lives? populated? neighbor-count)
+  (or (and populated? (or (unsafe-fx= neighbor-count 2)
+                          (unsafe-fx= neighbor-count 3)))
+      (and (not populated?)
+           (unsafe-fx= neighbor-count 3))))
+
+(module+ test
+  (check-eq? (lives? #f 0) #f)
+  (check-eq? (lives? #f 1) #f)
+  (check-eq? (lives? #f 2) #f)
+  (check-eq? (lives? #f 3) #t)
+  (check-eq? (lives? #f 4) #f)
+  (check-eq? (lives? #f 5) #f)
+  (check-eq? (lives? #f 6) #f)
+  (check-eq? (lives? #f 7) #f)
+  (check-eq? (lives? #f 8) #f)
+  (check-eq? (lives? #t 0) #f)
+  (check-eq? (lives? #t 1) #f)
+  (check-eq? (lives? #t 2) #t)
+  (check-eq? (lives? #t 3) #t)
+  (check-eq? (lives? #t 4) #f)
+  (check-eq? (lives? #t 5) #f)
+  (check-eq? (lives? #t 6) #f)
+  (check-eq? (lives? #t 7) #f)
+  (check-eq? (lives? #t 8) #f))
 
 (define (tick w)
-  (void))
+  (match-define (bit-world rows cols current next) w)
+  (for* ([x (in-range cols)]
+         [y (in-range rows)])
+    (define populated? (bit-worldv-ref current rows cols x y))
+    (define neighbor-count (count-neighbors current rows cols x y))
+    (define cell-lives? (lives? populated? neighbor-count))
+    (bit-worldv-set! next cols x y cell-lives?))
+  (set-bit-world-current! w next)
+  (set-bit-world-next! w current)
+  w)
 
-(module+ benchmark
+(module+ test
+  (define (check-tick w-str w2-str)
+    (define w  (string->bit-world w-str))
+    (define w2 (string->bit-world w2-str))
+    (define actual-w2 (tick w))
+    (define actual (bit-world-current actual-w2))
+    (define expected (bit-world-current w2))
+    (check-equal? actual expected)
+    (unless (equal? actual expected)
+      (beside (bit-world->image (string->bit-world w-str) #:label-neighbors #t)
+              (bit-world->image actual-w2)
+              (bit-world->image w2))))
+
+  (check-tick (string-append "...\n"
+                             "...\n"
+                             "...\n")
+              (string-append "...\n"
+                             "...\n"
+                             "...\n"))
+
+  (check-tick (string-append "...\n"
+                             ".X.\n"
+                             "...\n")
+              (string-append "...\n"
+                             "...\n"
+                             "...\n"))
+
+  (check-tick (string-append "...\n"
+                             "XXX\n"
+                             "...\n")
+              (string-append ".X.\n"
+                             ".X.\n"
+                             ".X.\n"))
+
+  (check-tick (string-append ".X.\n"
+                             ".X.\n"
+                             ".X.\n")
+              (string-append "...\n"
+                             "XXX\n"
+                             "...\n"))
   )
 
-(module+ visualizer
-  (require 2htdp/universe
-           2htdp/image)
-
-  (define SCALE 5)
+(define (bit-world->image w #:label-neighbors [lbl? #f])
+  (define SCALE 20)
   (define BOX (square SCALE "solid" "black"))
+  (match-define (bit-world rows cols current _) w)
+  (for*/fold ([img (empty-scene (* SCALE cols) (* SCALE rows))])
+             ([x (in-range cols)]
+              [y (in-range rows)])
+    (if (bit-worldv-ref current rows cols x y)
+        (place-image (if lbl?
+                         (overlay (text (number->string (count-neighbors current rows cols x y)) SCALE "white")
+                                  BOX)
+                         BOX)
+                     (+ (/ SCALE 2) 0.5 (* x SCALE))
+                     (+ (/ SCALE 2) 0.5 (* y SCALE))
+                     img)
+        (if lbl?
+            (overlay
+             (place-image
+              (text (number->string (count-neighbors current rows cols x y)) SCALE "black")
+              (+ (/ SCALE 2) 0.5 (* x SCALE))
+              (+ (/ SCALE 2) 0.5 (* y SCALE))
+              img)
+             img)
+            img))))
 
-  (define (draw w)
-    (match-define (bit-world rows cols current _) w)
-    (for*/fold ([img (empty-scene (* SCALE cols) (* SCALE rows))])
-               ([i (in-range rows)]
-                [j (in-range cols)])
-               (if (bit-world-ref current cols i j)
-                   (place-image SCALE
-                                (+ (/ SCALE 2) 0.5 (* j SCALE))
-                                (+ (/ SCALE 2) 0.5 (* i SCALE))
-                                img)
-                   img)))
+(module+ benchmark
+  ;; Jay's:
+  ;;      original: cpu time: 1843 real time: 1842 gc time: 36
+  ;; dishv-ref/set: cpu time: 1683 real time: 1682 gc time: 82
+  ;;     neighbors: cpu time:  530 real time:  531 gc time: 0
 
+  ;; Mine:
+  ;;      original: cpu time: 1655 real time: 1654 gc time: 2
+  ;; define-inline: cpu time: 1084 real time: 1083 gc time: 2
+  ;;
+
+  (define seed-str
+    "........................O...........
+    ......................O.O...........
+    ............OO......OO............OO
+    ...........O...O....OO............OO
+    OO........O.....O...OO..............
+    OO........O...O.OO....O.O...........
+    ..........O.....O.......O...........
+    ...........O...O....................
+    ............OO......................")
+
+  (define seed (string->bit-world seed-str))
+  (collect-garbage)
+  (collect-garbage)
+  (time
+   (for ([i (in-range 10000)])
+     (tick seed))))
+
+(module+ visualizer
   (big-bang (string->bit-world (port->string (open-input-file "glider.txt")))
             [on-tick tick]
-            [on-draw draw]))
+            [on-draw bit-world->image]))
