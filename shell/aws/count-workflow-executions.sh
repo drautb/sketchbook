d4ba0549332380f50ef7286603aee7a6f7210639
Ben Draut <drautb@gmail.com>
2019-05-02 08:10:25 -0600

    Update execution counter script

--- shell/aws/count-workflow-executions.sh
+++ shell/aws/count-workflow-executions.sh
@@ -3,14 +3,15 @@
 DOMAIN="$1"
 PROVISIONER="$2"
 VERSION="$3"
+TYPE="$4"
 
 PHASES=("check" "preDeploy" "bind" "deploy" "integrate" "unintegrate" "undeploy" "unbind" "postundeploy")
 ACCOUNTS=("fh1" "fh3" "fh5" "fh7")
 
 for account in "${ACCOUNTS[@]}"; do
-  printf "Counting open executions in %s:\\n" "$account"
+  printf "Counting $TYPE executions in %s:\\n" "$account"
   for phase in "${PHASES[@]}"; do
-    count=$(aws swf count-open-workflow-executions --domain "$DOMAIN" --start-time-filter oldestDate=0,latestDate="$(date +%s)" --type-filter name="$PROVISIONER.$phase",version="$VERSION" --profile="$account-us-east-1" | jq -r '.count')
+    count=$(aws swf count-$TYPE-workflow-executions --domain "$DOMAIN" --start-time-filter oldestDate=0,latestDate="$(date +%s)" --type-filter name="$PROVISIONER.$phase",version="$VERSION" --profile="$account-us-east-1" | jq -r '.count')
     echo "$phase: $count"
   done
   printf "\\n"
