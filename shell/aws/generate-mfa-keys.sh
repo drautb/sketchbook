50c23c0135069941510c9df51c61b5d961a24bfb
Ben Draut <drautb@gmail.com>
2019-06-23 13:47:19 -0600

    Helper script for MFA credentials

---
+++ shell/aws/generate-mfa-keys.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+creds=$(aws sts get-session-token --serial-number arn:aws:iam::$AWS_ACCOUNT_ID:mfa/$AWS_USERNAME --token-code "$1")
+
+access_key=$(echo "$creds" | jq -r .Credentials.AccessKeyId)
+secret_key=$(echo "$creds" | jq -r .Credentials.SecretAccessKey)
+token=$(echo "$creds" | jq -r .Credentials.SessionToken)
+
+cat << EOF > ~/dpt-playground-mfa-env
+export AWS_ACCESS_KEY_ID='$access_key'
+export AWS_SECRET_ACCESS_KEY='$secret_key'
+export AWS_SESSION_TOKEN='$token'
+EOF
