5b680c0151b1932a47b41e55732e60d3b62df160
Chris Roskelley <RoskelleyCJ@familysearch.org>
2019-09-18 07:35:21 -0600

    Allow regular expressions to help find blueprints
    I needed to locate all of the 'paas-sps' blueprints.
    I modified the extract-blueprint-names script to
    locate blueprints based on regular expressions to
    include and exclude.
    
    For example this is what I ran:
    ```
    ruby extract-blueprint-names.rb "paas-sps.*$" "(.*testapp.*)|(paas-sps.*-alarms)$"
    ```
    
    Which gave me pretty much all of the blueprints that 'deploy'
    for Provisioning.

--- ruby/github/blueprint-scraper/extract-blueprint-names.rb
+++ ruby/github/blueprint-scraper/extract-blueprint-names.rb
@@ -1,14 +1,24 @@
 require "yaml"
 
+bp_name_include = ARGV[0]
+bp_name_exclude = ARGV[1]
+
+bp_name_include = "^.*$" if bp_name_include.nil?
+
 blueprint_names = []
 
 Dir["fs-eng/*.yml"].each do |filename|
-  yml = YAML.load_file(filename)
-  puts filename
-  name = yml["name"]
+  begin
+    yml = YAML.load_file(filename)
+    puts filename
+    name = yml["name"]
 
-  if name != "" and name != nil  and yml["deploy"] != nil
-    blueprint_names << name
+    if name != "" and name != nil  and yml["deploy"] != nil
+      next if !bp_name_exclude.nil? && name =~ /#{bp_name_exclude}/
+      blueprint_names << name if name =~ /#{bp_name_include}/
+    end
+  rescue => e
+    # Don't care, skipping malformed blueprint.
   end
 end
 
