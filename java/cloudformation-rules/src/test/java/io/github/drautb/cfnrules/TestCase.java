355aafd36a8212fdb0025ff96d00a64fbd357365
Chris Roskelley <RoskelleyCJ@familysearch.org>
2019-10-05 16:01:29 -0600

    Allow more facts to be presented
    There will be times where an original input parameter
    can not be changed.  This commit adds the concept.

--- java/cloudformation-rules/src/test/java/io/github/drautb/cfnrules/TestCase.java
+++ java/cloudformation-rules/src/test/java/io/github/drautb/cfnrules/TestCase.java
@@ -1,8 +1,10 @@
 package io.github.drautb.cfnrules;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.jeasy.rules.api.Facts;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author drautb
@@ -12,5 +14,6 @@ public class TestCase {
   public String description;
   public JsonNode template;
   public List<String> errors;
+  public Map<String, Object> facts;
 
 }
