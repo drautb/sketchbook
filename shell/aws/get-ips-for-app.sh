33a675ee0e19f51fa9e859a2099196ce5a72bcc0
Ben Draut <drautb@gmail.com>
2021-02-10 10:02:04 -0700

    Update script

--- shell/aws/get-ips-for-app.sh
+++ shell/aws/get-ips-for-app.sh
@@ -3,9 +3,10 @@
 set -e
 
 APPLICATION_NAME=$1
+PROFILE=$2
 
-ENV_NAMES=($(aws elasticbeanstalk describe-environments --application-name "$APPLICATION_NAME" | jq -j '"\(.Environments[].EnvironmentName) "'))
+ENV_NAMES=($(aws elasticbeanstalk describe-environments --application-name "$APPLICATION_NAME" --profile "$PROFILE" | jq -j '"\(.Environments[].EnvironmentName) "'))
 
 for env in "${ENV_NAMES[@]}"; do
-  aws ec2 describe-instances --filters "Name=tag:Name,Values=$env" | jq -r '.Reservations[].Instances[].PrivateIpAddress'
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=$env" --profile "$PROFILE" | jq -r '.Reservations[].Instances[].PrivateIpAddress'
 done
