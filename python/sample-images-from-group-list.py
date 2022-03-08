ab9fa4744987bbd1dfd5eb6d34ac43574009e3cb
Ben Draut <drautb@gmail.com>
2022-03-08 14:10:52 -0700

    Add basic sampling script

---
+++ python/sample-images-from-group-list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import random
+import sys
+import pandas as pd
+
+FILE=sys.argv[1]
+COUNT=int(sys.argv[2])
+SEED=int(sys.argv[3])
+
+if SEED:
+    random.seed(SEED)
+
+df = pd.read_csv(FILE)
+
+groups = df.sample(COUNT, replace=True, weights=df['Image_Count'], random_state=SEED if SEED else random.getstate())
+for _, row in groups.iterrows():
+    image = random.randint(1, row["Image_Count"])
+    print(f'{row["Group"]}_{image:05d}')
+
