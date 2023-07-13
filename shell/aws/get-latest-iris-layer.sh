6f5abe5def5241269af2b166145e0dc3fd8ac0bb
Ben Draut <drautb@amazon.com>
2023-07-13 17:23:43 +0000

    Add script

---
+++ shell/aws/get-latest-iris-layer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+stage="$1"
+site="$2"
+layer="$3"
+
+tmpfile="$(mktemp)"
+
+aws s3 cp --quiet "s3://ariadne-iris-$stage-$site/${site}_latest_snapshot_completion_notification.json" \
+  "$tmpfile" \
+  --profile "$site"
+
+prefix="$(< "$tmpfile" jq -r '.key_prefix')"
+
+aws s3 cp --quiet "s3://ariadne-iris-$stage-$site/$prefix/$layer.json" \
+  "$tmpfile" \
+  --profile "$site"
+
+/usr/bin/cat "$tmpfile"
+
+
